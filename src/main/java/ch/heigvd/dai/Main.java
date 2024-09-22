19167b212da5446907dd8ccba5b06bea7e77f0c3
Syseria <nicocarbo@live.fr>
2024-09-22 14:59:46 +0200

    Went further with Spotless

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,38 +1,38 @@
 package ch.heigvd.dai;
 
-import ch.heigvd.dai.commands.Hello;
 import ch.heigvd.dai.commands.Goodbye;
+import ch.heigvd.dai.commands.Hello;
 import java.io.File;
 import picocli.CommandLine;
 
 @CommandLine.Command(
-        description = "A small CLI with subcommands to demonstrate picocli.",
-        version = "1.0.0",
-        subcommands = {
-                Hello.class,
-                Goodbye.class,
-        },
-        scope = CommandLine.ScopeType.INHERIT,
-        mixinStandardHelpOptions = true
-)
-
+    description = "A small CLI with subcommands to demonstrate picocli.",
+    version = "1.0.0",
+    subcommands = {
+      Hello.class,
+      Goodbye.class,
+    },
+    scope = CommandLine.ScopeType.INHERIT,
+    mixinStandardHelpOptions = true)
 public class Main {
 
-    @CommandLine.Parameters(
-            index = "0",
-            description = "The name of the user (default: World).",
-            defaultValue = "World"
-    ) protected String name;
+  @CommandLine.Parameters(
+      index = "0",
+      description = "The name of the user (default: World).",
+      defaultValue = "World")
+  protected String name;
 
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public static void main (String[] args) {
-        String jarFilename = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
+  public static void main(String[] args) {
+    String jarFilename =
+        new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+            .getName();
 
-        int exitCode = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
+    int exitCode = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
 
-        System.exit(exitCode);
-    }
+    System.exit(exitCode);
+  }
 }
--- src/main/java/ch/heigvd/dai/commands/Goodbye.java
+++ src/main/java/ch/heigvd/dai/commands/Goodbye.java
@@ -5,23 +5,22 @@ import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
 @CommandLine.Command(
-        name = "goodbye",
-        description = "Print a 'Goodbye World!' type of message.",
-        mixinStandardHelpOptions = true
-)
+    name = "goodbye",
+    description = "Print a 'Goodbye World!' type of message.",
+    mixinStandardHelpOptions = true)
 public class Goodbye implements Callable<Integer> {
 
-    @CommandLine.ParentCommand protected Main parent;
+  @CommandLine.ParentCommand protected Main parent;
 
-    @CommandLine.Option(
-            names = {"-f", "--farewells"},
-            description = "The farewells to address the user (default: Goodbye).",
-            defaultValue = "Goodbye"
-    ) protected String farewells;
+  @CommandLine.Option(
+      names = {"-f", "--farewells"},
+      description = "The farewells to address the user (default: Goodbye).",
+      defaultValue = "Goodbye")
+  protected String farewells;
 
-    @Override
-    public Integer call() {
-        System.out.println(farewells + " " + parent.getName() + "!");
-        return 0;
-    }
+  @Override
+  public Integer call() {
+    System.out.println(farewells + " " + parent.getName() + "!");
+    return 0;
+  }
 }
